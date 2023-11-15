2dfb2230a796e5b05282b70c2510d040fae5f94e
joab001 <56304133+joab001@users.noreply.github.com>
2023-11-15 10:59:26 -0300

    emplementando logica de mudança de formularios

--- js/script.js
+++ js/script.js
@@ -3,13 +3,31 @@ const poiter = document.querySelector('.poiter');
 const formUsres = document.querySelector('.form-usres');
 const formCompanies = document.querySelector('.form-companies');
 
+const registrationWindow = document.querySelector('.registration-window');
+const loginWindow = document.querySelector('.login-window');
+const createAccount = document.querySelector('.new-account');
 
-slider.addEventListener('click',(event)=>{
-    if(event='click'){
-        poiter.classList.toggle('active');
-        formUsres.classList.toggle('none');
-        formCompanies.classList.toggle('none');
-    }
+const logo = document.querySelector('.white-logo');
+
+
+logo.addEventListener('click',()=>{});
+
+slider.addEventListener('click',()=>{
+    poiter.classList.toggle('active');
+    formUsres.classList.toggle('none');
+    formCompanies.classList.toggle('none');
+});
+
+createAccount.addEventListener('click',()=>{
+     registrationWindow.classList.toggle('none');
+    loginWindow.classList.toggle('none');
 });
 
 
+
+
+
+
+// function openRegistrationWindow() {
+   
+// }
